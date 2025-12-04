6e88f6315322c47377669fbc9337a78318d36751
Jackson Markey <jackson.markey@gmail.com>
2025-12-04 12:16:43 -0500

    day 4 part 2

--- day4.py
+++ day4.py
@@ -27,19 +27,36 @@ def rolls_adjacent(input: list[list[str]], y: int, x: int) -> int:
             acc += 1
     return acc
 
-def accessible_rolls(input: list[list[str]]) -> int:
-    acc = 0
+def accessible_rolls(input: list[list[str]]) -> list[(int, int)]:
+    acc = []
     for y in range(len(input)):
         for x in range(len(input[0])):
             if input[y][x] == ROLL and rolls_adjacent(input, y, x) < 4:
-                acc += 1
+                acc.append((y, x))
     return acc
 
 
 def main():
     with open("printing.txt") as file:
-        input = [l.rstrip() for l in file.readlines()]
-        print(accessible_rolls(input))
+        # part 1
+        input = [list(l.rstrip()) for l in file.readlines()]
+        accessible = accessible_rolls(input)
+        print(len(accessible))
+        # part 2
+        removed = 0
+        while True:
+            removed += len(accessible)
+            for y, x in accessible:
+                input[y][x] = 'x'
+            new_accessible = accessible_rolls(input)
+            if len(new_accessible) == 0:
+                break
+            accessible = new_accessible
+        print(removed)
+        
+
+
+
 
 if __name__=="__main__":
     main()
