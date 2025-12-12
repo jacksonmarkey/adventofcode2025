c2ed34f27b3a6fe1ecaabc778bf7a6717e836530
Jackson Markey <jackson.markey@gmail.com>
2025-12-12 11:56:31 -0500

    day 1 part 2

--- day1.py
+++ day1.py
@@ -1,18 +1,29 @@
 def main():
     counter = 50
-    acc = 0
+    pt_1_clicks = 0
+    pt_2_clicks = 0
     with open("secret.txt") as file:
         for line in file:
             direction = line[0]
             distance = int(line[1:])
-            print(f"{direction}{distance}")
+            # print(f"counter:{counter}")
+            # print(f"{direction}{distance}")
             if direction == "L":
-                counter -= distance
+                new_counter = counter - distance
             else:
-                counter += distance
-            if counter % 100 == 0:
-                acc += 1
-    print(acc)
+                new_counter = counter + distance
+            if new_counter % 100 == 0:
+                pt_1_clicks += 1
+            # print(f"new_counter:{new_counter}")
+            # print(f"add:{abs(new_counter // 100 - counter // 100)}")
+            pt_2_clicks += abs(new_counter // 100 - counter // 100)
+            if counter % 100 == 0 and direction == "L":
+                pt_2_clicks -= 1
+            if new_counter % 100 == 0 and direction == "L":
+                pt_2_clicks += 1
+            counter = new_counter
+    print(pt_1_clicks)
+    print(pt_2_clicks)
 
 
 if __name__=="__main__":
